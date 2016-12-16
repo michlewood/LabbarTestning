3eb7811a485cf08ea70c984c03e5accde85f5c5d
Michlewood <michlewood@hotmail.com>
2016-12-16 15:38:28 +0100

    gjorde 2 uppgifter att söka i google automatiskt och att köpa i Lyko

---
+++ CSharpGoogleSelenium/CSharpGoogleSelenium/Program.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpGoogleSelenium
+{
+    // TODO 1. Skapa en main
+    // TODO 2. hämta plugin
+    // TODO 3. Hitta sökfältet
+    // TODO 4. Sök efter "Testautomatisering Stockholm"
+    // TODO 5. Hitta sök knappen klicka på den
+    // TODO 6. Ta fram resultatstats och skriv ut de
+    class Program
+    {
+        // Skapa en variabel som hållerwebdriver
+        static IWebDriver driver;
+        // Wait map
+        //static IWait<IWebDriver> wait;
+
+        static void Main(string[] args)
+        {
+            // Ställa in driver till att använda Firefox
+            driver = new FirefoxDriver();
+            // Öppna google
+            driver.Url = "http://www.google.com";
+            // Ställa in webdrivewait med driver den ska använda och antalet sekunder den ska vänta
+            //wait = new WebDriverWait(driver, 3);
+            Thread.Sleep(2000);
+            // Hitta sök fältet och skriv in "Testautomatisering Stockholm"
+            driver.FindElement(By.Id("lst-ib")).SendKeys("Testautomatisering Stockholm");
+            // Hitta sök knappen, klicka på den
+            driver.FindElement(By.Id("_fZl")).Click();
+            // Vänta tills dess att elementet är synligt
+            Thread.Sleep(2000);
+            IWebElement results = driver.FindElement(By.Id("resultStats"));
+            // Ta fram resultatstats och skriv ut
+            Console.WriteLine(results.Text);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Övning3 - Lyko/Övning3/Program.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Övning3
+{
+    class Program
+    {
+        static IWebDriver driver;
+        static void Main(string[] args)
+        {
+            driver = new FirefoxDriver();
+
+            driver.Url = "http://www.Lyko.se";
+            Thread.Sleep(2000);
+
+            driver.FindElement(By.Name("q")).SendKeys("Schampoo");
+            Thread.Sleep(3000);
+            driver.FindElement(By.ClassName("A69wSs")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.CssSelector("div._1Nvacq:nth-child(1) > button:nth-child(2) > span:nth-child(1)")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.CssSelector("._1w9fSZ")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.CssSelector("a._3itYIP:nth-child(1)")).Click();
+            Thread.Sleep(3000);
+            for (int i = 0; i < 9; i++)
+            {
+                driver.FindElement(By.CssSelector(".YwJvmF")).Click(); 
+            }
+            Thread.Sleep(1000);
+
+            if (driver.FindElement(By.CssSelector(".jmb20U")).Displayed)
+            {
+                Console.WriteLine("Loggan fanns");
+                driver.FindElement(By.CssSelector(".jmb20U")).Click();
+            }
+            else
+            {
+                Console.WriteLine("Loggan fanns inte");
+            }
+        }
+    }
+}

191a6d4dd9aa44637f51f54ce9dab1e8941e038d
Michlewood <michlewood@hotmail.com>
2016-12-22 10:46:51 +0100

    förlängde sleep värdena

--- Övning3 - Lyko/Övning3/Program.cs
+++ Övning3 - Lyko/Övning3/Program.cs
@@ -20,20 +20,21 @@ namespace Övning3
             Thread.Sleep(2000);
 
             driver.FindElement(By.Name("q")).SendKeys("Schampoo");
-            Thread.Sleep(3000);
+            Thread.Sleep(4000);
             driver.FindElement(By.ClassName("A69wSs")).Click();
             Thread.Sleep(3000);
             driver.FindElement(By.CssSelector("div._1Nvacq:nth-child(1) > button:nth-child(2) > span:nth-child(1)")).Click();
-            Thread.Sleep(3000);
+            Thread.Sleep(4000);
             driver.FindElement(By.CssSelector("._1w9fSZ")).Click();
             Thread.Sleep(3000);
             driver.FindElement(By.CssSelector("a._3itYIP:nth-child(1)")).Click();
             Thread.Sleep(3000);
             for (int i = 0; i < 9; i++)
             {
+                Thread.Sleep(200);
                 driver.FindElement(By.CssSelector(".YwJvmF")).Click(); 
             }
-            Thread.Sleep(1000);
+            Thread.Sleep(3000);
 
             if (driver.FindElement(By.CssSelector(".jmb20U")).Displayed)
             {

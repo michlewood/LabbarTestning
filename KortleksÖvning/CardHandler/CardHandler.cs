cbab88b1a6204a4f44d296e2c77e1833e744d7a6
Michlewood <michlewood@hotmail.com>
2017-01-20 11:25:04 +0100

    gjort CheckValue

--- KortleksÖvning/CardHandler/CardHandler.cs
+++ KortleksÖvning/CardHandler/CardHandler.cs
@@ -49,9 +49,42 @@ namespace CardHandlerlib
                 return "Club";
             else if (input == 'h' || input == 'H')
                 return "Heart";
-            else if (input == 's' ||input == 'S')
+            else if (input == 's' || input == 'S')
                 return "Spade";
             else return "Invalid input";
+        }
+
+        public string ValuesChecker(string input)
+        {
+            //return "2";
+
+            if (input.Contains("10"))
+                return "10";
+            else if (input.Contains("9"))
+                return "9";
+            else if (input.Contains("8"))
+                return "8";
+            else if (input.Contains("7"))
+                return "7";
+            else if (input.Contains("6"))
+                return "6";
+            else if (input.Contains("5"))
+                return "5";
+            else if (input.Contains("4"))
+                return "4";
+            else if (input.Contains("3"))
+                return "3";
+            else if (input.Contains("2"))
+                return "2";
+            else if (input.Contains("1"))
+                return "Ace";
+            else if (input.ToUpper().Contains("J"))
+                return "Jack";
+            else if (input.ToUpper().Contains("Q"))
+                return "Queen";
+            else if (input.ToUpper().Contains("K"))
+                return "King";
+            else return "Invalid value";
         }
     }
 }
--- KortleksÖvning/CardHandlerTester_NUnit/Class1.cs
+++ KortleksÖvning/CardHandlerTester_NUnit/Class1.cs
@@ -9,9 +9,9 @@ using CardHandlerlib;
 namespace CardHandlerTester_NUnit
 {
     [TestFixture]
-    public class Class1
+    public class SuitChecker
     {
-        
+
         [Test]
         public void Test_If_Input_Is_d_Returns_Diamond()
         {
@@ -50,6 +50,70 @@ namespace CardHandlerTester_NUnit
             var expectedOutput = "Club";
             CardHandler class1 = new CardHandler();
             var actualOutput = class1.SuitChecker(input);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+
+    [TestFixture]
+    public class CheckValues
+    {
+        [Test]
+        public void Test_If_Input_Is_2_Returns_2()
+        {
+            var input = "2";
+            var expectedOutput = "2";
+            CardHandler class1 = new CardHandler();
+            var actualOutput = class1.ValuesChecker(input);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [Test]
+        public void Test_If_Input_Is_10_Returns_10()
+        {
+            var input = "2";
+            var expectedOutput = "2";
+            CardHandler class1 = new CardHandler();
+            var actualOutput = class1.ValuesChecker(input);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [Test]
+        public void Test_If_Input_Is_1_Returns_Ace()
+        {
+            var input = "1";
+            var expectedOutput = "Ace";
+            CardHandler class1 = new CardHandler();
+            var actualOutput = class1.ValuesChecker(input);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [Test]
+        public void Test_If_Input_Is_J_Returns_Jack()
+        {
+            var input = "J";
+            var expectedOutput = "Jack";
+            CardHandler class1 = new CardHandler();
+            var actualOutput = class1.ValuesChecker(input);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [Test]
+        public void Test_If_Input_Is_q_Returns_Queen()
+        {
+            var input = "q";
+            var expectedOutput = "Queen";
+            CardHandler class1 = new CardHandler();
+            var actualOutput = class1.ValuesChecker(input);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [Test]
+        public void Test_If_Input_Is_e_Returns_Invalid_value()
+        {
+            var input = "e";
+            var expectedOutput = "Invalid value";
+            CardHandler class1 = new CardHandler();
+            var actualOutput = class1.ValuesChecker(input);
             Assert.AreEqual(expectedOutput, actualOutput);
         }
     }
